31e73d8a75ba657433b96817527d6b5b124d8123
Wian Lewies <lewieswian@gmail.com>
2024-04-19 20:54:25 +0200

    Travel commit

--- COS132/Practicals/Practical 4/Task 2/DigitBasedPasswordCracker.cpp
+++ COS132/Practicals/Practical 4/Task 2/DigitBasedPasswordCracker.cpp
@@ -1,5 +1,5 @@
 #include "DigitBasedPasswordCracker.h"
-
+#include "Printer.h"
 
 int crackPassswordPerDigit(int password, int numDigits){
     return crackDigit(password, guessedDigit = 0, numDigitsLeftToGuess = numDigits, currentPasswordGuess = 0)
@@ -7,14 +7,16 @@ int crackPassswordPerDigit(int password, int numDigits){
 
 // Modified processDigitGuess function
 int processDigitGuess(int password, int guessedDigit, int numDigitsLeftToGuess, int currentPasswordGuess) {
-    int extractedDigit;
-    if (numDigitsLeftToGuess == 4) {
-        
-    }
+    int extractedDigit = password[(password.length - numDigitsLeftToGuess)];
+    (guessedDigit == extractedDigit) ? 
+    return crackDigit(password, guessedDigit, numDigitsLeftToGuess - 1, currentPasswordGuess) :
+    return crackDigit(password, guessedDigit + 1, numDigitsLeftToGuess, currentPasswordGuess);
 }
 
 
 // Modified crackDigit function to use processDigitGuess
 int crackDigit(int password, int guessedDigit, int numDigitsLeftToGuess, int currentPasswordGuess) {
     printCurrentGuess(password, guessedDigit, numDigitsLeftToGuess, currentPasswordGuess);
-    numDigitsLeftToGuess == 0 ? return guessedDigit : return processDigitGuess(password, guessedDigit, numDigitsLeftToGuess, currentPassswordGuess);
+    numDigitsLeftToGuess == 0 ?
+    return guessedDigit :
+    return processDigitGuess(password, guessedDigit, numDigitsLeftToGuess, currentPassswordGuess);
--- COS132/Practicals/Practical 4/Task 2/RangeBasedPasswordCracker.cpp
+++ COS132/Practicals/Practical 4/Task 2/RangeBasedPasswordCracker.cpp
@@ -2,12 +2,18 @@
 #include "Printer.h"
 
 int decideAndSearch(int guess, int password, int low, int high) {
-    isLessThan(guess, password) ? crackPassword(guess + 1, high, password) : isGreaterThan(guess, password) ? crackPassword(low, guess - 1, password) : return password;
+    isLessThan(guess, password) ? 
+    crackPassword(guess + 1, high, password) : 
+    (isGreaterThan(guess, password) ? 
+    crackPassword(low, guess - 1, password) : 
+    password);
 }
 
 
 int crackPassword(int low, int high, int password) {
     int guess = (int) (low + high) / 2; // The midpoint between high and low floored.
     printGuess(midpoint);
-    isEqual(password, guess) ? foundPassword() : decideAndSearch();    
+    isEqual(password, guess) ? 
+    foundPassword() : 
+    decideAndSearch();    
 }
