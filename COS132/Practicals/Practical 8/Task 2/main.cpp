23cb87c0c94fa746a645372a0bf1886a6661c572
Wian Lewies <lewieswian@gmail.com>
2024-05-24 13:55:59 +0200

    Removes Zone Identifiers

---
+++ COS132/Practicals/Practical 8/Task 2/main.cpp
@@ -0,0 +1,14 @@
+#include "RoomLocater.h"
+#include "BombDefusing.h"
+#include <cassert>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    // Add your tests here
+    // You can use assert for tests
+    return 0;
+}
